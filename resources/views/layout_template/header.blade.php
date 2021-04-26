adb694d3382d03b6d763c728019aeaac414ba0ff
Hien Anh <h.anhtr15@gmail.com>
2021-04-26 14:53:25 +0700

    account navigation

--- resources/views/layout_template/header.blade.php
+++ resources/views/layout_template/header.blade.php
@@ -29,7 +29,15 @@
                         <a class="nav-link @if(url()->current() == route('feedback')) active @endif" href="{{route('feedback')}}">Feedback</a>
                     </li>
 
-                    <li class="nav-item"> <a class="nav-link" href="javascript: void(0)" data-toggle="modal" data-target="#sureLogout">Logout</a> </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Hello, Hien Anh!</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="javascript:void(0)">Account Settings</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="javascript:void(0)" data-toggle="modal" data-target="#sureLogout">Log out</a>
+                        </div>
+                    </li>
+
                     @else
                     <li class="nav-item"> <a class="nav-link" href="javascript:void(0)" data-toggle="modal" data-target="#modalLogin">Log in </a> </li>
                     @endif
