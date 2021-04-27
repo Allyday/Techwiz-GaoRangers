3fd7f6419d3cb1e1b9cc89c36bd44dce9e375119
Hien Anh <h.anhtr15@gmail.com>
2021-04-27 11:34:27 +0700

    rearrange fields

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -18,6 +18,10 @@
          <div class="row">
             <!-- REGISTER -->
             <div class="col-md-12">
+               <div class="text-center w-100 p-4">
+                  <h1 class="page-title">Become a member</h1>
+                  <p>Register with us to enjoy over 2000 dishes from 300+ different restaurants delivered to your doorstep, all just a mouse-click away.</p>
+               </div>
 
                <form action="{{ route('auth.save') }}" method="POST">
                   @csrf
@@ -46,26 +50,17 @@
                            <input name="firstName" required class="form-control" type="text" placeholder="Your first name" id="FirstName" value="{{ old('firstName') }}" @error('firstName')style=" border-color:red;" @enderror>
                         </div>
 
-                        <div class="form-group col-md-12">
+                        <div class="form-group col-md-6">
                            <div style="display: flex; justify-content: space-between;">
-                              <label for="Email">Email address </label>
-                              @error('mail')
+                              <label for="Phonenumber">Phone number </label>
+                              @error('phoneNumber')
                               <p class="text-danger">{{ $message }}</p>
                               @enderror
                            </div>
-                           <input name="mail" value="{{ old('mail') }}" required type="email" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Enter email" @error('mail')style=" border-color:red;" @enderror> <small id="emailHelp" class="form-text text-muted">We"ll never share your email with anyone else.</small>
+                           <input name="phoneNumber" value="{{ old('phoneNumber') }}" required class="form-control" type="text" placeholder="0988666888" id="Phonenumber" @error('phoneNumber')style=" border-color:red;" @enderror>
                         </div>
 
-                        <div class="form-group col-md-12">
-                           <div style="display: flex; justify-content: space-between;">
-                              <label for="Username">Username </label>
-                              @error('userName')
-                              <p class="text-danger">{{ $message }}</p>
-                              @enderror
-                           </div>
-                           <input name="userName" value="{{ old('userName') }}" required type="text" class="form-control" id="Username" placeholder="Username" @error('userName')style=" border-color:red;" @enderror>
-                        </div>
-                        <div class="form-group col-md-12">
+                        <div class="form-group col-md-6">
                            <label>Gender <i class="optional-label text-muted">(Optional)</i></label>
                            <select class="custom-select w-100" name="gender">
                               <option value="male" selected>Male</option>
@@ -74,6 +69,15 @@
                            </select>
                         </div>
 
+                        <div class="form-group col-md-12">
+                           <div style="display: flex; justify-content: space-between;">
+                              <label for="Email">Email address </label>
+                              @error('mail')
+                              <p class="text-danger">{{ $message }}</p>
+                              @enderror
+                           </div>
+                           <input name="mail" value="{{ old('mail') }}" required type="email" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Enter email" @error('mail')style=" border-color:red;" @enderror> <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
                      </div>
                      <div class="col-md-6">
                         <div class="form-group col-md-12">
@@ -89,15 +93,15 @@
 
                         <div class="form-group col-md-12">
                            <div style="display: flex; justify-content: space-between;">
-                              <label for="Phonenumber">Phone number </label>
-                              @error('phoneNumber')
+                              <label for="Username">Username </label>
+                              @error('userName')
                               <p class="text-danger">{{ $message }}</p>
                               @enderror
                            </div>
-                           <input name="phoneNumber" value="{{ old('phoneNumber') }}" required class="form-control" type="text" placeholder="0988666888" id="Phonenumber" @error('phoneNumber')style=" border-color:red;" @enderror> <small class="form-text text-muted">We"ll never share your phone number with anyone else.</small>
+                           <input name="userName" value="{{ old('userName') }}" required type="text" class="form-control" id="Username" placeholder="Username" @error('userName')style=" border-color:red;" @enderror>
                         </div>
 
-                        <div class="form-group col-md-12">
+                        <div class="form-group col-md-6">
                            <div style="display: flex; justify-content: space-between;">
                               <label for="Password">Password </label>
                               @error('password')
@@ -107,7 +111,7 @@
                            <input name="password" value="{{ old('password') }}" required type="password" class="form-control" id="Password" placeholder="Password" @error('password')style=" border-color:red;" @enderror>
                         </div>
 
-                        <div class="form-group col-md-12">
+                        <div class="form-group col-md-6">
                            <div style="display: flex; justify-content: space-between;">
                               <label for="Repeatpassword">Repeat password </label>
                               @if(Session::get('repeatPass'))
